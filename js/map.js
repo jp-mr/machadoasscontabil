2b1385d8fe0d7fb57f234cff1efb1140eda3f514
Michel <michel.rodrigues86@yahoo.com.br>
2016-08-19 18:34:21 -0300

    Google Mapa .embed-responsive-16by9 {

--- js/map.js
+++ js/map.js
@@ -5,14 +5,14 @@ var onMapMouseleaveHandler = function(event) {
     var that = $(this);
     that.on('click', onMapClickHandler);
     that.off('mouseleave', onMapMouseleaveHandler);
-    that.find('div').css("pointer-events", "none");
+    that.find('iframe').css("pointer-events", "none");
 }
 var onMapClickHandler = function(event) {
   var that = $(this);
   // Disable the click handler until the user leaves the map area
   that.off('click', onMapClickHandler);
   // Enable scrolling zoom
-  that.find('div').css("pointer-events", "auto");
+  that.find('iframe').css("pointer-events", "auto");
   // Handle the mouse leave event
   that.on('mouseleave', onMapMouseleaveHandler);
 }
@@ -20,39 +20,39 @@ var onMapClickHandler = function(event) {
 $('.map').on('click', onMapClickHandler);
 
 
-// Google Map API
+//// Google Map API
+
+//function initMap(){
+//  var location = new google.maps.LatLng(-22.9241528,-45.46106629999997)
+//  var mapCanvas = document.getElementById('gmap_canvas')
 //
-function initMap(){
-  var location = new google.maps.LatLng(-22.9241528,-45.46106629999997)
-  var mapCanvas = document.getElementById('gmap_canvas')
-
-  var myOptions = {
-    zoom:18,
-    center:location,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  var map = new google.maps.Map(mapCanvas, myOptions);
-
-  var markerImage = 'img/marker.png';
-
-  marker = new google.maps.Marker({
-    map: map,
-    position: location,
-    icon: markerImage,
-    maxWidth: 500
-  });
-
-  var contentString = '<strong>Mult Tecnologia - UNIDADE: Pindamonhagaba</strong><br>Av. Cel. Fernando Prestes, 60 - Centro, Pindamonhangaba - SP, 12400-240<br>';
-
-  infowindow = new google.maps.InfoWindow({
-    content: contentString,
-  });
-  
-  google.maps.event.addListener(marker, 'click', function(){
-    infowindow.open(map,marker);
-  });
-  
-  infowindow.open(map,marker);
-}
-google.maps.event.addDomListener(window, 'load', initMap);
+//  var myOptions = {
+//    zoom:18,
+//    center:location,
+//    mapTypeId: google.maps.MapTypeId.ROADMAP
+//  };
+//
+//  var map = new google.maps.Map(mapCanvas, myOptions);
+//
+//  var markerImage = 'img/marker.png';
+//
+//  marker = new google.maps.Marker({
+//    map: map,
+//    position: location,
+//    icon: markerImage,
+//    maxWidth: 500
+//  });
+//
+//  var contentString = '<strong>Mult Tecnologia - UNIDADE: Pindamonhagaba</strong><br>Av. Cel. Fernando Prestes, 60 - Centro, Pindamonhangaba - SP, 12400-240<br>';
+//
+//  infowindow = new google.maps.InfoWindow({
+//    content: contentString,
+//  });
+//  
+//  google.maps.event.addListener(marker, 'click', function(){
+//    infowindow.open(map,marker);
+//  });
+//  
+//  infowindow.open(map,marker);
+//}
+//google.maps.event.addDomListener(window, 'load', initMap);
